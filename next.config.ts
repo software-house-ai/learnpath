d05ba7e68f5e23bda158450ba2c6946b9e4669d9
M-UmarJaved <muhammadumarjaved1325@gmail.com>
2026-03-05 23:14:05 +0500

    fix(ci): fix lint configuration using next.config dirs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    dirs: ['app', 'components', 'lib'],
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
